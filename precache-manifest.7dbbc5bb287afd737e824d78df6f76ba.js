a3e81b8d73a8d4f74e0f95d4566e22805a491884
marco Koch <marco.koch@uni-saarland.de>
2020-09-22 20:30:54 +0200

    deploy

---
+++ precache-manifest.7dbbc5bb287afd737e824d78df6f76ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d4a8e43d0ac6729aba",
+    "url": "/oboe-tools/css/app.f842c2ac.css"
+  },
+  {
+    "revision": "45f69907a0b8b21b3058858d92e0ba25",
+    "url": "/oboe-tools/index.html"
+  },
+  {
+    "revision": "45d4a8e43d0ac6729aba",
+    "url": "/oboe-tools/js/app.c434fa9f.js"
+  },
+  {
+    "revision": "4be754d0abf73e13595b",
+    "url": "/oboe-tools/js/chunk-vendors.992b00fa.js"
+  },
+  {
+    "revision": "23eb0ee30351e42d5fceb0606ea33ebb",
+    "url": "/oboe-tools/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/oboe-tools/robots.txt"
+  }
+]);
